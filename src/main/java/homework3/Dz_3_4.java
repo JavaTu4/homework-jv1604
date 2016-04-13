caa6a36f7139b1a6d2fe0c3d1e7647ef16a794cf
tu4 <tu4e4ka096@gmail.com>
2016-04-13 18:22:00 +0300

    changed dz3

---
+++ src/main/java/homework3/Dz_3_4.java
@@ -0,0 +1,37 @@
+package homework3;
+
+/** Created by tu4 on 4/6/16. */
+public class Dz_3_4 {
+    public static void main(String[] args) {
+        int [][] array = new int[10][];
+        for (int i = 0;i < array.length;i++){
+           // for (int j = 1;j < array.length;j++){
+            array[i] = new int[i + 1];
+            //}
+        }
+        int k = 0;
+        for (int a = 0;a < array.length;a++){
+            for (int b = 0;b < a + 1;b++){
+                array[a][b] = k;
+                k++;
+            }
+        }
+        for (int a = 0;a < array.length;a++) {
+            for (int b = 0; b < a + 1; b++) {
+                System.out.print(array[a][b] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int a = array.length - 1;a >= 0;a--) {
+            for (int b = 0; b < a + 1; b++) {
+                System.out.print(array[a][b] + "\t");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+
+    }
+}
